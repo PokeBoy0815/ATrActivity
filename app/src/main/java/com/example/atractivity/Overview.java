a5496471cef71e6e20b27b8d13afd9a53981982d
Markus <markus.weinberger@stud.uni-regensburg.de>
2020-09-25 12:59:42 +0200

    Freitag 25. 13:00

--- app/src/main/java/com/example/atractivity/Overview.java
+++ app/src/main/java/com/example/atractivity/Overview.java
@@ -1,6 +1,7 @@
 package com.example.atractivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -17,4 +18,10 @@ public class Overview extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
