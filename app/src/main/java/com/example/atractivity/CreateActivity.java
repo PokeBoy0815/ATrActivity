4fc0d4c338038b9766f5f8ce0aea24dd10b4bbfa
Markus <markus.weinberger@stud.uni-regensburg.de>
2020-09-25 10:27:21 +0200

    Freitag 25. 10:27

--- app/src/main/java/com/example/atractivity/CreateActivity.java
+++ app/src/main/java/com/example/atractivity/CreateActivity.java
@@ -2,23 +2,28 @@ package com.example.atractivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-
 public class CreateActivity extends AppCompatActivity {
+
+
+        private FloatingActionButton notificationButton;
+        private EditText activityName;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.createactivity);
 
-            FloatingActionButton fab = findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
+            activityName = findViewById(R.id.nameofactivity);
+
+            notificationButton = findViewById(R.id.fab);
+            notificationButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
