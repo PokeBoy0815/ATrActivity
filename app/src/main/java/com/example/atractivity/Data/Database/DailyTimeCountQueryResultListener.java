3932543b39336716d343f320ca9d006f5fbf158c
Markus <markus.weinberger@stud.uni-regensburg.de>
2020-10-10 19:05:24 +0200

    Samstag 19:05

--- app/src/main/java/com/example/atractivity/Data/Database/DailyTimeCountQueryResultListener.java
+++ app/src/main/java/com/example/atractivity/Data/Database/DailyTimeCountQueryResultListener.java
@@ -1,11 +1,9 @@
 package com.example.atractivity.Data.Database;
 
-import com.example.atractivity.Data.ActivityItem;
-
 import java.util.List;
 
 public interface DailyTimeCountQueryResultListener {
 
     void onListResult(List<DaylyTimeCount> timeCounts);
-    void onTimeResult(int minutes);
+    int onTimeResult(int minutes);
 }
